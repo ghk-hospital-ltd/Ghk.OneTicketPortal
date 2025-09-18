5344f4d37ce92d9529e3386a8022a7ce4155a8ae
Eric Chiu <eric.chiu@gleneagles.hk>
2025-09-17 21:18:23 -0700

    T&C

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { TicketStatusPage } from './ticket-status.page';
 import { PaymentSuccessComponent } from './payment.success.component';
 import { PaymentFailurePage } from './payment.failure.component';
+import { TermsPage } from './term-and-conditions.page';
  export const routes: Routes = [
   { path: 'ticketStatus/ticket', component: TicketStatusPage },
   { path: 'payment/success', component: PaymentSuccessComponent },
     { path: 'payment/failure', component: PaymentFailurePage },
+      { path: 'terms', component: TermsPage },
   { path: '', redirectTo: '/ticket', pathMatch: 'full' }, // Redirect root to /ticket
-  { path: '**', redirectTo: '/ticket' }
+  { path: '**', redirectTo: '/ticket' },
+
 ];
